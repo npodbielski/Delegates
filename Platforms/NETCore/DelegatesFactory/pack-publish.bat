f4e8856bbe74b5525912d549336cb1ce2cb6629a
npodbielski <n.podbielski@gmail.com>
2017-01-26 20:35:08 +0100

    Moved nuget files. Removed Nuget project

--- Platforms/NETCore/DelegatesFactory/pack-publish.bat
+++
@@ -1,2 +0,0 @@
-nuget pack Nuget.nuspec -symbols -properties Configuration=Release -outputdirectory bin\Release
-nuget push .\bin\Release\DelegatesFactory.1.0.0.nupkg -Source https://www.nuget.org/api/v2/package
