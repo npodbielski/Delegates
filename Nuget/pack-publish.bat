e3dcd01e2e2cf5b726cdfcb5e578bd7eb50d331b
npodbielski <n.podbielski@gmail.com>
2017-10-04 00:17:29 +0200

    New package version

--- Nuget/pack-publish.bat
+++ Nuget/pack-publish.bat
@@ -1,3 +1,4 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild" c:\OneDrive\Delegates\Delegates.sln /t:rebuild /p:configuration=Release
 nuget pack Nuget.nuspec -symbols -properties Configuration=Release -outputdirectory packages
-nuget push .\packages\DelegatesFactory.1.0.1.nupkg -Source https://www.nuget.org/api/v2/package
-nuget push .\packages\DelegatesFactory.1.0.1.symbols.nupkg -source https://nuget.smbsrc.net/
+nuget push .\packages\DelegatesFactory.1.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push .\packages\DelegatesFactory.1.1.0.symbols.nupkg -source https://nuget.smbsrc.net/
